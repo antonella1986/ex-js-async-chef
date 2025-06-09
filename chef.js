632510ef9c890e12fa520ae1367c1adb2f3fe501
antonella1986 <antonella.leo.mail@gmail.com>
2025-06-09 17:18:06 +0200

    edit const names

--- chef.js
+++ chef.js
@@ -8,8 +8,8 @@ async function getChefBirthday(id) {
             throw new Error('Ricetta non trovata :c');
         }
 
-        const ricetta = await response.json()
-        const userId = ricetta.userId;
+        const recipe = await response.json()
+        const userId = recipe.userId;
         console.log('Id ricevuto:', userId)
 
         const responseChef = await fetch(`https://dummyjson.com/users/${userId}`);
@@ -23,11 +23,11 @@ async function getChefBirthday(id) {
         const formattedBd = dayjs(birthday).format('DD/MM/YYYY')
 
         return formattedBd
-    } catch (error) {
+    } catch(error) {
         console.error('Errore', error.message)
     }
 }
 
 getChefBirthday(1)
-  .then(birthday => console.log("Data di nascita dello chef:", birthday))
+  .then(formattedBd => console.log("Data di nascita dello chef:", formattedBd))
   .catch(error => console.error("Errore:", error.message));
